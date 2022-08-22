2078babf119e59d5e9cb0f85a8da6045c784a7dc
Ryan-marcus <104573558+Ryan-marcus@users.noreply.github.com>
2022-08-22 09:37:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var blockImageObject= "";
 
 function newImage(getImage)
 {
-	fabric.Image.fromURL(get_image,function (Img) {
+	fabric.Image.fromURL(getImage,function (Img) {
 		blockImageObject=Img;
 
 		blockImageObject.scaleToWidth(blockImageWidth);
@@ -33,32 +33,32 @@ console.log(keyPressed);
 
 	if(keyPressed == '69') // adicione os códigos adequados às teclas
 	{
-		new_image("rr1.png");
+		newImage("rr1.png");
 		console.log("r")
 	}
 	if(keyPressed == '86')
 	{
 		blockX = 200;
-		new_image("gr.png");
+		newImage("gr.png");
 		console.log("g")
 	}
 	
 	if(keyPressed == '65')
 	{
 		blockX =350;
-		new_image("yr.png");
+		newImage("yr.png");
 		console.log("y")
 	}
 	if(keyPressed == '82')
 	{
 		blockX = 600;
-		new_image("pr.png");
+		newImage("pr.png");
 		console.log("p")
 	}
 	if(keyPressed == '73')
 	{
 		blockX = 700;
-		new_image("br.png");
+		newImage("br.png");
 		console.log("b")
 	}
 	
